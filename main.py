ff77b51a6fbe8c21a55e9787669c5aa638ed883b
Farkhatzhan Makhmudov <faralost@gmail.com>
2022-12-11 21:56:36 +0600

    init commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import logging
+from telethon import TelegramClient, events
+from config import API_ID, API_HASH
+
+logging.basicConfig(level=logging.WARNING, filename='logs.txt')
+
+client = TelegramClient("redirect", API_ID, API_HASH)
+
+from_chats = ('findwork', 'devkz_jobs', 'lost_test')
+to_chat = "redirect_posts_bot"
+tags = ('Python',)
+
+
+@client.on(events.NewMessage)
+async def my_event_handler(event):
+    chat = await event.get_chat()
+    if chat.username in from_chats:
+        for tag in tags:
+            if tag in event.message.message:
+                await client.forward_messages(to_chat, event.message)
+
+
+if __name__ == '__main__':
+    client.start()
+    print('Bot has started!')
+    client.run_until_disconnected()
